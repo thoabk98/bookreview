cb1740f3bac2c425f1dd88ccafc0eee9c4b3950b
Quang DUy <quangduybk95@gmail.com>
2019-10-29 23:54:57 +0900

    update user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
   def update
-    new_params = params.require(:user).permit(:email, :username, :current_password, :password, :password_confirmation, profile_attributes: [:bio, :avatar, :sex])
     change_password = true
     if params[:user][:password].blank?
       params[:user].delete("password")
       params[:user].delete("password_confirmation")
-      new_params = params.require(:user).permit(:email, :username)
       change_password = false
     end
 
@@ -13,9 +11,9 @@ class RegistrationsController < Devise::RegistrationsController
     is_valid = false
 
     if change_password
-      is_valid = @user.update_with_password(new_params)
+      is_valid = @user.update_with_password(sign_up_params)
     else
-      is_valid = @user.update_without_password(new_params)
+      is_valid = @user.update_without_password(sign_up_params)
     end
 
     if is_valid
@@ -28,6 +26,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
     private
     def sign_up_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, profile_attributes: [:bio, :avatar, :sex])
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one :profile
+  has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,:omniauthable
