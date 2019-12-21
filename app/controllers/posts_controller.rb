cf103d73b7e84f62ad7b1829405f4d91193d65b7
thoabk98 <hoangthithoa09041998@gmail.com>
2019-12-22 00:03:45 +0700

    fix view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,6 @@ class PostsController < ApplicationController
     if params[:category_id]
       @category = Category.find params[:category_id]
       @posts = @category.posts
-    else
-      @posts = Post.all
     end 
   end
 
